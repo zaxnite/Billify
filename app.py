86ef20afdf1457db9fb075a91b082828103feb35
Aathif Khan <65447745+zaxnite@users.noreply.github.com>
2024-07-20 15:14:54 +0400

    Created More Pages

--- app.py
+++ app.py
@@ -177,6 +177,21 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 @app.route('/login')
 def login():
     clear_cache()
